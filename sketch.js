12c493070b2cbd8e44bed2abfaafd2d5b018c78b
lucaszz1 <148370485+lucaszz1@users.noreply.github.com>
2023-11-22 13:31:09 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background("rgb(0,0,0)");
+  fill("#0079DA");
+  textSize(64);
+  textAlign(CENTER, CENTER);
+  
+  let maximo = width;
+  let minimo = 0;
+  let palavra = "lucas";
+  let quantidade = map(mouseX, 0, width, 1, palavra.length);
+  let parcial = palavra.substring(0, quantidade)
+  text(parcial, 200, 200);
+}
